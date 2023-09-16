710eb895e8242a5657f1df979a96c578c28834ba
Gustavo Malcotti <gustavo@malcotti.com>
2023-09-15 21:01:32 -0300

    1

---
+++ clase_05/intjs/js/scripts.js
@@ -0,0 +1,23 @@
+//alert('Quiero Pizza y cerveza')
+
+let nombre_usuario = "Juan";
+let edad = 25;
+let estudia = false; // true
+
+//nombre = "Maria";
+
+//alert(nombre_usuario);
+
+const a = 22;
+let b = 2;
+
+let c = a % b;
+
+//console.log(c);
+
+let btn1 = document.getElementById('btn1');
+let resultado = document.getElementById('resultado');
+
+btn1.addEventListener('click', ()=>{
+    resultado.innerHTML = "<h1>FIN</h1>";
+})

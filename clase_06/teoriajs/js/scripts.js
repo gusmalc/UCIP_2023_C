721ef76f67e36b3eba214baa1fb2f95f415a974e
Gustavo Malcotti <gustavo@malcotti.com>
2023-09-22 20:59:02 -0300

    1

---
+++ clase_06/teoriajs/js/scripts.js
@@ -0,0 +1,57 @@
+let nombre = "222";
+
+const n = 25;
+
+let dias = ['Lunes','Martes','Miercole',2023]; // array
+
+
+// alert(typeof nombre);
+
+// console.log(dias);
+
+let persona = {
+    nombre: "Maria",
+    edad: 35,
+    estudia: true,
+    saluda: function(){
+        alert('Hola soy Maria')
+    }
+};
+
+let val = "edad";
+
+//persona.saluda();
+
+//console.log(persona[val]);
+
+//console.log(persona.edad);
+
+// function saludar(){
+//     alert('HOLA');
+// }
+// saludar(); 
+
+
+// function sumar(a,b){
+//     let x = parseInt(a);
+//     let y = parseFloat(b);
+//     alert(x+y);
+// }
+
+
+// sumar("aaaa",'5.3');
+
+let x = 20;
+let y = '20';
+
+// if(x > y){
+//     alert('X es mayor que Y');
+// }else{
+//     alert('X es menor que Y');
+// }
+
+if(x === y){
+    alert('X es igual que Y');
+}else{
+    alert('X no es igual que Y');
+}
